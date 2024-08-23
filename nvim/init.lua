f9338733f2c515c3060986d185d3e9b97ae61ec1
Riley Watkins <riley@rileywatkins.com>
2024-08-23 14:38:59 -0700

    Neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+-- This is my neovim config.
+-- There are many like it, but this one is mine.
+
+-- Imagine not using space as your leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Hybrid relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Accentuate the current line number
+vim.opt.cursorline = true
+
+-- Basic tab stuff to avoid tab characters
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
