3913be083fa08b299c870b54c8a4f16c47db90d0
Riley Watkins <riley@rileywatkins.com>
2024-08-23 16:17:33 -0700

    Neovim: add mini.deps

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,3 +17,22 @@ vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 
+-- Clone mini.nvim manually in a way that it gets managed by mini.deps
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim',
+    mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up mini.deps
+require('mini.deps').setup({ path = { package = path_package } })
