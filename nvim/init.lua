32e5378ce1bc25d31f6a172244f3312afd687dd9
Riley Watkins <riley@rileywatkins.com>
2024-08-25 11:47:39 -0700

    Neovim updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,10 +12,21 @@ vim.opt.relativenumber = true
 -- Accentuate the current line number
 vim.opt.cursorline = true
 
--- Basic tab stuff to avoid tab characters
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Trim trailing whitespace on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  callback = function (ev)
+    save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
 
 -- Clone mini.nvim manually in a way that it gets managed by mini.deps
 local path_package = vim.fn.stdpath('data') .. '/site/'
@@ -34,5 +45,48 @@ if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
+-- After search highlights all of the occurrences, pressing Esc removes the highlight.
+vim.cmd("nnoremap <silent> <esc> :noh<cr><esc>")
+
 -- Set up mini.deps
 require('mini.deps').setup({ path = { package = path_package } })
+
+-- Colorscheme
+MiniDeps.add('ellisonleao/gruvbox.nvim')
+require("gruvbox").setup({
+  italic = {
+    strings = false,
+    emphasis = false,
+    comments = false,
+    operators = false,
+    folds = false,
+  }
+})
+vim.opt.background = 'dark'
+vim.cmd('colorscheme gruvbox')
+
+-- Telescope
+MiniDeps.add({
+  source = 'nvim-telescope/telescope.nvim',
+  depends = { 'nvim-lua/plenary.nvim' },
+})
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>t', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+
+-- Treesitter
+MiniDeps.add({
+  source = 'nvim-treesitter/nvim-treesitter',
+  hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+})
+
+-- vim-sleuth
+MiniDeps.add('tpope/vim-sleuth')
+
+-- Gitsigns
+MiniDeps.add('lewis6991/gitsigns.nvim')
+require('gitsigns').setup()
